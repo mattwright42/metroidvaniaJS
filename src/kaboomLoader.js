26ff5e205f4a6afdc79faaa26c186cf28e5b03a0
Matthew Wright <matthewbwright@gmail.com>
2024-07-25 21:41:36 -0700

    adds the info for loadSprite

--- src/kaboomLoader.js
+++ src/kaboomLoader.js
@@ -10,3 +10,39 @@ export const k = kaboom({
 });
 
 k.loadFont("glyphmesss", "./assets/glyphmesss.ttf");
+
+k.loadSprite("player", "./assets/sprites/u.png", {
+  sliceX: 8,
+  sliceY: 9,
+  anims: {
+    idle: { from: 0, to: 7, loop: true },
+    run: { from: 8, to: 13, loop: true },
+    jump: { from: 51, to: 51, loop: true },
+    fall: { from: 54, to: 54, loop: true },
+    explode: { from: 64, to: 69 },
+    attack: { from: 24, to: 28, speed: 16 },
+  },
+});
+
+k.loadSprite("drone", "./assets/sprites/dr0ne.png", {
+  sliceX: 6,
+  sliceY: 3,
+  anims: {
+    flying: { from: 0, to: 3, loop: true },
+    attack: { from: 6, to: 11, loop: true },
+    explode: { from: 12, to: 17 },
+  },
+});
+
+k.loadSprite("burner", "./assets/sprites/burn3r.png", {
+  sliceX: 5,
+  sliceY: 6,
+  anims: {
+    idle: { from: 0, to: 3, loop: true },
+    run: { from: 6, to: 8, loop: true },
+    "open-fire": { from: 10, to: 14 },
+    fire: { from: 15, to: 18, loop: true },
+    "shut-fire": { from: 20, to: 23 },
+    explode: { from: 25, to: 29 },
+  },
+});
